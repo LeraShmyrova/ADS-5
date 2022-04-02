1d325253d656c895d560ddefb7eb1aa8098d63a0
LeraShmyrova <94710005+LeraShmyrova@users.noreply.github.com>
2022-04-02 23:57:21 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,9 +3,88 @@
 #include <map>
 #include "tstack.h"
 
+int operacii(char op){
+  int c = 0;  
+  if (op == '(') {
+  c = 0;  
+  }else if (op == ')') {
+  c = 1;
+  }else if (op == '+') {
+  c = 2;
+  }else if (op == '-') {
+  c = 2;
+  }else if (op == '*') {
+  c = 3; 
+  }else if (op == '/') {
+  c = 3;  
+  }else if (op == ' ') {
+  c = 4;
+  }else {
+  c = 5;
+  }
+   return c;
+  }
+
+int vichislenie(int a, int b, char op){
+  int res = 0;
+  if (op == '+') {
+  res = b + a;
+  }else if(op == '-') {
+  res = b - a;
+  }else if(op == '*') {
+  res = b * a;
+  }else if((op == '/') && (a != 0)) {
+  res = b / a;
+  }else {
+  res = 0;
+  }
+  return res;
+}
+
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+  std::string peremenaya;  
+  TStack <char,100> TS;
+  int i = 0;
+  while (i < inf.size()) {
+  if (operacii(inf[i] == 5) {
+  peremenaya.push_back(inf[i]);
+  peremenaya.push_back(' ');
+  }else{
+  if (operacii(inf[i] == 0) {
+  TS.push(inf[i]);
+  }else if (operacii(inf[i] == 1) {
+  while (operacii(TS.get()) != 0) {
+  peremenaya.push_back(TS.get());
+  peremenaya.push_back(' '); 
+  TS.pop();  
+  }
+  TS.pop();
+  }else if ((operacii(inf[i])) > (operacii(TS.get()))) {
+  TS.push(inf[i]);
+  }else if(TS.isEmpty()) {
+  TS.push(inf[i]);
+  }else{
+   while ((!TS.isEmpty()) && ((operacii(inf[i])) <= (operacii(TS.get())))) {
+   peremenaya.push_back(TS.get());
+   peremenaya.push_back(' '); 
+   TS.pop();
+   } 
+   TS.push(inf[i]); 
+  }
+  }
+  }
+  while (!TS.isEmpty()) {
+  peremenaya.push_back(TS.get());
+  peremenaya.push_back(' '); 
+  TS.pop();
+  }
+  int j = 0;
+  while (j < peremenaya.size()) {
+  if (peremenaya[peremenaya.size() - 1] == ' ') {
+   peremenaya.erase(peremenaya.size() - 1); 
+  i++;  
+  }       
+  return peremenaya;
 }
 
 int eval(std::string pref) {
