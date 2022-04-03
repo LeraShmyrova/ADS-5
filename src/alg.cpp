bc8aa34ca8214db40ec26a8c839fd0eb32c18a74
LeraShmyrova <94710005+LeraShmyrova@users.noreply.github.com>
2022-04-03 13:53:59 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,23 +3,23 @@
 #include <map>
 #include "tstack.h"
 
-int operacii(char op){
-  int c = 0;  
+int operacii(char op) {
+  int c = 0;
   if (op == '(') {
-  c = 0;  
-  }else if (op == ')') {
+  c = 0;
+  } else if (op == ')') {
   c = 1;
-  }else if (op == '+') {
+  } else if (op == '+') {
   c = 2;
-  }else if (op == '-') {
+  } else if (op == '-') {
   c = 2;
-  }else if (op == '*') {
-  c = 3; 
-  }else if (op == '/') {
-  c = 3;  
-  }else if (op == ' ') {
+  } else if (op == '*') {
+  c = 3;
+  } else if (op == '/') {
+  c = 3;
+  } else if (op == ' ') {
   c = 4;
-  }else {
+  } else {
   c = 5;
   }
    return c;
@@ -29,72 +29,72 @@ int vichislenie(int a, int b, char op){
   int res = 0;
   if (op == '+') {
   res = b + a;
-  }else if(op == '-') {
+  } else if(op == '-') {
   res = b - a;
-  }else if(op == '*') {
+  } else if(op == '*') {
   res = b * a;
-  }else if((op == '/') && (a != 0)) {
+  } else if((op == '/') && (a != 0)) {
   res = b / a;
-  }else {
+  } else {
   res = 0;
   }
   return res;
 }
 
-std::string infx2pstfx(std::string inf) {
-  std::string peremenaya;  
+std::string infx2pstfx(std::string inf){
+  std::string peremenaya;
   TStack <char,100> TS;
   int i = 0;
   while (i < inf.size()) {
   if (operacii(inf[i] == 5)) {
   peremenaya.push_back(inf[i]);
   peremenaya.push_back(' ');
-  }else{
-  if (operacii(inf[i] == 0) {
+  } else {
+  if (operacii(inf[i] == 0)) {
   TS.push(inf[i]);
-  }else if (operacii(inf[i] == 1) {
+  } else if (operacii(inf[i] == 1) {
   while (operacii(TS.get()) != 0) {
   peremenaya.push_back(TS.get());
-  peremenaya.push_back(' '); 
-  TS.pop();  
+  peremenaya.push_back(' ');
+  TS.pop();
   }
   TS.pop();
-  }else if ((operacii(inf[i])) > (operacii(TS.get()))) {
+  } else if ((operacii(inf[i])) > (operacii(TS.get()))) {
   TS.push(inf[i]);
   }else if(TS.isEmpty()) {
   TS.push(inf[i]);
-  }else{
+  } else {
    while ((!TS.isEmpty()) && ((operacii(inf[i])) <= (operacii(TS.get())))) {
    peremenaya.push_back(TS.get());
-   peremenaya.push_back(' '); 
+   peremenaya.push_back(' ');
    TS.pop();
-   } 
-   TS.push(inf[i]); 
+   }
+   TS.push(inf[i]);
   }
   }
   }
   while (!TS.isEmpty()) {
   peremenaya.push_back(TS.get());
-  peremenaya.push_back(' '); 
+  peremenaya.push_back(' ');
   TS.pop();
   }
   int j = 0;
   while (j < peremenaya.size()) {
   if (peremenaya[peremenaya.size() - 1] == ' ') {
-   peremenaya.erase(peremenaya.size() - 1); 
-  i++;  
-  }       
+   peremenaya.erase(peremenaya.size() - 1);
+  i++;
+  }
   return peremenaya;
 }
 
-int eval(std::string pref) {
+int eval(std::string pref){
   TStack <int,100> TSt;
   int result = 0;
   int i = 0;
   while (i < pref.size()) {
   if (operacii(pref[i] == ) {
   TSt.push(pref[i] - '0');
-  }else if (operacii(pref[i] < 4) {
+  } else if (operacii(pref[i] < 4) {
   int a = TSt.get();
   TSt.pop();
   int b = TSt.get();
